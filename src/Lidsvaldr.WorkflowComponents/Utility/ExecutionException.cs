1848d7679a454032ea26e78cc3ed6f2d022c751b
kustova <solitarymite@gmail.com>
2018-01-06 21:58:46 +0700

    Added comments(documentation) to ExecutionException

--- src/Lidsvaldr.WorkflowComponents/Utility/ExecutionException.cs
+++ src/Lidsvaldr.WorkflowComponents/Utility/ExecutionException.cs
@@ -4,14 +4,35 @@ using System.Text;
 
 namespace Lidsvaldr.WorkflowComponents.Utility
 {
+    /// <summary>
+    /// Represents a custom exception which occuring while node function execution.
+    /// </summary>
     public class ExecutionException : Exception
     {
+        /// <summary>
+        /// Source node name.
+        /// </summary>
         private string _sourceName;
+        /// <summary>
+        /// Arguments which was passed to the node function.
+        /// </summary>
         private object[] _args;
 
+        /// <summary>
+        /// Source node name.
+        /// </summary>
         public string SourceName => _sourceName;
+        /// <summary>
+        /// Arguments which was passed to the node function.
+        /// </summary>
         public object[] ExecutionArguments => _args;
 
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="source">Name of source node.</param>
+        /// <param name="args">Arguments which was passed to the node function</param>
+        /// <param name="inner">Inner exception.</param>
         internal ExecutionException(string source, object[] args, Exception inner) 
             : base(ComponentsResources.ExecutionException, inner)
         {
